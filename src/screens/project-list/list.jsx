8d97b143f7ec801b1767cbbe3c4a359020d77e32
TravisWongX <travis.x.wong@gmail.com>
2021-10-28 17:51:46 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/list.jsx
@@ -0,0 +1,16 @@
+export const List = ({list, users}) => {
+  return <table>
+    <thead>
+    <tr>
+      <td>项目</td>
+      <td>负责人</td>
+    </tr>
+    </thead>
+    <tbody>
+    {list.map(project => <tr>
+      <td>{project.name}</td>
+      <td>{users.find(user => user.id === project.personId)?.name || '未找到'}</td>
+    </tr>)}
+    </tbody>
+  </table>
+}
