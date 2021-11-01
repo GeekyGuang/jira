22881652ea9d5e12f7c640b54ef13b9e213f0a7a
TravisWongX <travis.x.wong@gmail.com>
2021-11-01 17:37:36 +0800

    feat: 添加登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import { LoginScreen } from "./screens/login";
 // import { ProjectListScreen } from "./screens/project-list";
-import { TsReactTest } from "try-use-array";
 
 function App() {
   return (
     <div className="App">
-      <TsReactTest />
+      <LoginScreen />
       {/*<ProjectListScreen />*/}
     </div>
   );
